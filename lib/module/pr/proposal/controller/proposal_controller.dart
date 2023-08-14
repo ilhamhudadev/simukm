001f06f76bbab46fbac2c577d171054ed7779d7e
Nor Aisa <abdullahnordin20@gmail.com>
2023-08-14 21:25:39 +0700

    update proposal

--- lib/module/pr/proposal/controller/proposal_controller.dart
+++ lib/module/pr/proposal/controller/proposal_controller.dart
@@ -1,6 +1,145 @@
 
   import 'package:get/get.dart';
   import 'package:standard_project/module/pr/proposal/data/repo/proposal_repo.dart';
+  import 'package:standard_project/module/pr/proposal/data/model/user_management.dart';
+
   class ProposalController extends GetxController with ProposalRepo {
-  
+    
+    List<proposal1> proposalList = [
+      
+      proposal1(
+        
+        Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+      
+      proposal1(
+        
+        Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+
+proposal1(
+        
+        Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+      
+      proposal1(
+        
+        Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+
+      proposal1(
+        
+         Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+      
+      proposal1(
+        
+        Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+
+      proposal1(
+        
+         Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+      
+      proposal1(
+        
+         Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+
+      proposal1(
+        
+         Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+
+      proposal1(
+        
+         Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+
+      proposal1(
+        
+         Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+
+      proposal1(
+        
+         Judul: "Ketahanan Energi Nasional",
+        Deskripsi: "Seminar",
+        Tanggal: "2018-11-03",
+        Waktu: "9.00 WIB s/d 13.00 WIB",
+        Tempat: "Jakarta",
+        Anggaran: "Rp 2.500.000",
+        Dokumen: "Ketahanan_Energi_Nasional.pdf",
+      ),
+
+
+
+    ];
   }
