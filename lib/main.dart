e4f96f5526bd9bd6ea704fbfe2cecc363b468525
ilhamhuda <ilham.huda@istyle.id>
2023-12-30 16:10:37 +0700

    [UPDATE] Update Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:standard_project/core/globalcontroller/app_function.dart';
 import 'package:standard_project/core/localization/app_language.dart';
 import 'package:standard_project/core/network/status_network_binding.dart';
 import 'package:standard_project/core/style/app_color.dart';
@@ -8,32 +10,37 @@ import 'core/route/app_route.dart';
 import 'core/route/route_constant.dart';
 
 void main() {
-  // getLocale().
-  //then((locale) {
-  runApp(const MainScreen(
-    appLocale: Locale('en'),
-  ));
-  // });
+  runApp(MainScreen());
 }
 
 class MainScreen extends StatelessWidget {
-  const MainScreen({
-    super.key,
-    required this.appLocale,
-  });
-  final Locale? appLocale;
-
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      color: AppColors.placeholdertextfield,
-      initialBinding: StatusNetworkBinding(),
-      translations: AppLanguage(),
-      locale: appLocale,
-      getPages: AppRoute.all,
-      initialRoute: RouteConstant.sidebar,
-      debugShowCheckedModeBanner: false,
-      defaultTransition: Transition.rightToLeft,
+    return FutureBuilder<bool>(
+      future: _isLoggedIn(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return GetMaterialApp(
+            color: AppColors.placeholdertextfield,
+            initialBinding: StatusNetworkBinding(),
+            translations: AppLanguage(),
+            locale: Locale('en'), // You can set the default locale here
+            getPages: AppRoute.all,
+            initialRoute: snapshot.data ?? false
+                ? RouteConstant.sidebar
+                : RouteConstant.signIn,
+            debugShowCheckedModeBanner: false,
+            defaultTransition: Transition.rightToLeft,
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
     );
+  }
+
+  Future<bool> _isLoggedIn() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('is_logged_in') ?? false;
   }
 }
